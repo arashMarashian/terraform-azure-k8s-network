3d4aa26b9181d076c6c5b7b1381dbb85a6f0135b
Arash Marashian <36083308+arashMarashian@users.noreply.github.com>
2026-01-15 21:41:30 +0200

    Add variables for resource group, location, and IP range

--- terraform-azure-network/variables.tf
+++ terraform-azure-network/variables.tf
@@ -1,1 +1,15 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
 
+variable "location" {
+  type        = string
+  description = "Azure region for resources"
+  default     = "westeurope"
+}
+
+variable "office_ip_range" {
+  type        = string
+  description = "IP range for office access (CIDR notation)"
+}
