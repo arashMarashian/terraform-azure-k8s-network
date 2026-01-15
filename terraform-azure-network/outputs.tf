c21da6ce6a8751374fb15fc0646fe1cf9320444b
Arash Marashian <36083308+arashMarashian@users.noreply.github.com>
2026-01-15 21:42:03 +0200

    Add outputs for virtual network and NSG IDs

--- terraform-azure-network/outputs.tf
+++ terraform-azure-network/outputs.tf
@@ -1,1 +1,9 @@
+output "virtual_network_id" {
+  description = "The ID of the Virtual Network"
+  value       = azurerm_virtual_network.vnet.id
+}
 
+output "network_security_group_id" {
+  description = "The ID of the Network Security Group"
+  value       = azurerm_network_security_group.k8s_nsg.id
+}
